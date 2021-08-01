bab5212023e34167c7d03108f52cb5ecb9d71386
JoaoGuiBC <joaoguibc@gmail.com>
2021-07-31 23:55:26 -0300

    Configured styled-components

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { ThemeProvider } from 'styled-components/native';
+
+import { Dashboard } from './src/screens/Dashboard';
+import theme from './src/global/styles/theme';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <ThemeProvider theme={theme}>
+        <StatusBar style="auto" />
+        <Dashboard />
+      </ThemeProvider>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#3c3c7e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
