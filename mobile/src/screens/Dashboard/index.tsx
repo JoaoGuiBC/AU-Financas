4a944bd360f7e167688123620d0270dbd0abc302
JoaoGuiBC <joaoguibc@gmail.com>
2021-08-01 15:42:43 -0300

    Header structure (HTML + CSS)

--- mobile/src/screens/Dashboard/index.tsx
+++ mobile/src/screens/Dashboard/index.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Icon,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+} from './styles';
 
 export const Dashboard: React.FC = () => {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{ uri: 'https://avatars.githubusercontent.com/u/64855040?v=4' }}
+            />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Jonas</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+
+        </UserWrapper>
+      </Header>
+
     </Container>
   );
 }
