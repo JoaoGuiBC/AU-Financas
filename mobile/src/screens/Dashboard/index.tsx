df9bfb251456659b6c8c1056ef2c41eecb5e292d
JoaoGuiBC <joaoguibc@gmail.com>
2021-08-04 18:00:45 -0300

    Dynamic HighlightCard values

--- mobile/src/screens/Dashboard/index.tsx
+++ mobile/src/screens/Dashboard/index.tsx
@@ -25,39 +25,93 @@ export interface DataListProps extends TransactionCardData {
   id: string;
 }
 
+interface HighlightProps {
+  amount: string;
+}
+
+interface HighlightData {
+  entries: HighlightProps;
+  expenses: HighlightProps;
+  total: HighlightProps;
+}
+
 export const Dashboard: React.FC = () => {
   const [transactions, setTransactions] = useState<DataListProps[]>([]);
+  const [highlightData, setHighlightData] = useState<HighlightData>({
+    entries: {
+      amount: "R$ 0"
+    },
+    expenses: {
+      amount: "R$ 0"
+    },
+    total: {
+      amount: "R$ 0"
+    },
+  } as HighlightData);
 
-  useEffect(() => {
-    async function loadTransactions() {
-      const response = await AsyncStorage.getItem('@aufinancas:transactions');
-      const data = response ? JSON.parse(response) : [];
+  async function loadTransactions() {
+    const response = await AsyncStorage.getItem('@aufinancas:transactions');
+    const data = response ? JSON.parse(response) : [];
 
-      const formattedData: DataListProps[] = data.map((transaction: DataListProps) => {
-        const amount = Number(transaction.amount).toLocaleString('pt-BR', {
-          style: 'currency',
-          currency: 'BRL'
-        });
+    let entriesTotal = 0;
+    let expenseTotal = 0;
 
-        const date = Intl.DateTimeFormat('pt-BR', {
-          day: '2-digit',
-          month: '2-digit',
-          year: '2-digit',
-        }).format(new Date(transaction.date));
+    const formattedData: DataListProps[] = await data.map((transaction: DataListProps) => {
+      if (transaction.type === "deposit") {
+        entriesTotal += Number(transaction.amount);
+      } else {
+        expenseTotal += Number(transaction.amount);
+      }
 
-        return {
-          id: transaction.id,
-          name: transaction.name,
-          amount,
-          type: transaction.type,
-          category: transaction.category,
-          date,
-        }
+      const amount = Number(transaction.amount).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
       });
 
-      return setTransactions(formattedData);
-    }
-    
+      const date = Intl.DateTimeFormat('pt-BR', {
+        day: '2-digit',
+        month: '2-digit',
+        year: '2-digit',
+      }).format(new Date(transaction.date));
+
+      return {
+        id: transaction.id,
+        name: transaction.name,
+        amount,
+        type: transaction.type,
+        category: transaction.category,
+        date,
+      }
+    });
+
+    const transactionsTotal = entriesTotal - expenseTotal
+
+    setHighlightData({
+      entries: {
+        amount: entriesTotal.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }),
+      },
+      expenses: {
+        amount: expenseTotal.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }),
+      },
+      total: {
+        amount: transactionsTotal.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }),
+      },
+    });
+    setTransactions(formattedData);
+
+    console.log(highlightData);
+  }
+
+  useEffect(() => {
     loadTransactions();
   }, []);
 
@@ -87,19 +141,19 @@ export const Dashboard: React.FC = () => {
       >
         <HighlightCard
           title="Entradas"
-          amount="R$ 17.400,00"
+          amount={highlightData.entries.amount}
           lastTransaction="Última entrada dia 13 de abril"
           type="deposit"
         />
         <HighlightCard
           title="Saídas"
-          amount="R$ 1.259,00"
+          amount={highlightData.expenses.amount}
           lastTransaction="Última saída dia 03 de abril"
           type="withdraw"
         />
         <HighlightCard
           title="Total"
-          amount="R$ 16.141,00"
+          amount={highlightData.total.amount}
           lastTransaction="01 á 16 de abril"
           type="total"
         />
