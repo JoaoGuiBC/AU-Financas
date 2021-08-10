9d2486d0a9cd634e638870f34a0914c6db9c8b3b
JoaoGuiBC <joaoguibc@gmail.com>
2021-08-09 21:58:34 -0300

    HistoryCard basic structure (HTML + CSS)

---
+++ mobile/src/components/HistoryCard/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {
+  Container,
+  Title,
+  Amount,
+} from './styles';
+
+interface HistoryCardProps {
+  title: string;
+  amount: string;
+  color: string;
+}
+
+const HistoryCard: React.FC<HistoryCardProps> = ({ title, amount, color }) => {
+  return (
+    <Container color={color}>
+      <Title>{title}</Title>
+      <Amount>{amount}</Amount>
+    </Container>
+  );
+}
+
+export { HistoryCard };
