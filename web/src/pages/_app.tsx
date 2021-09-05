875b13821a09873eb6ce7e8390f3bd09a59fc4e9
JoaoGuiBC <joaoguibc@gmail.com>
2021-09-04 23:47:20 -0300

    Configured styled-components

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,5 +1,19 @@
+import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyle } from '../styles/global';
+import theme from '../styles/theme';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Head>
+        <title>A&amp;U Finanças - web</title>
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp
